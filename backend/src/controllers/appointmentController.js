cea34557a91b0068950951470a765065430b657a
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2023-10-06 23:49:28 +0200

    add getAppoint

--- backend/src/controllers/appointmentController.js
+++ backend/src/controllers/appointmentController.js
@@ -11,11 +11,11 @@ const getAppointments= async(req,res)=>{
     const dateSearch= "2023-10-06"  //khod input men el front end
     if(statusInput==="Pending" || statusInput==="Done"){
     const statusValue=(statusInput==="Pending"?false:true)
-    const result= current_type==="Doctor"?await appointmentModel.find({Doccurrent_usertorUsername, Status:statusValue}) :await appointmentModel.find({PatientUsername:current_user,Status:statusValue}) 
+    const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user, Status:statusValue}) :await appointmentModel.find({PatientUsername:current_user,Status:statusValue}) 
     res.status(200).send(result)
 }
     else if(dateSearch!=null && !isNaN(new Date(dateSearch))){            //Gets date greater than or equal 
-        const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user,createdAt:{$gte:new Date(dateSearch)}}) : await appointmentModel.find({PatientUsername:current_user, createdAt:new Date(dateSearch)})
+        const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user,Date:new Date(dateSearch)}) : await appointmentModel.find({PatientUsername:current_user, Date:new Date(dateSearch)})
         res.status(200).send(result)
     }
   
