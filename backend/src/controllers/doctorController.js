a1ea3c9bc7fc74c9b6cdcdb7ba39a9d4ddaf9c3f
kholoudkhaleddaly <127565809+kholoudkhaleddaly@users.noreply.github.com>
2023-10-06 16:15:59 +0200

    updateemailthroghdoc
    update email through doctor

--- backend/src/controllers/doctorController.js
+++ backend/src/controllers/doctorController.js
@@ -1,5 +1,6 @@
 const doctorModel = require("../models/doctors");
 const mongoose= require("mongoose")
+const systemUserModel = require("../models/systemusers");
 
 
 const createDoctor = async(req,res) => {
@@ -17,15 +18,24 @@ const createDoctor = async(req,res) => {
 
 
 const updateDoctor = async(req,res) => {
-    if(req.body.HourlyRate || req.body.Affiliation){
-    const filter={Username:"Touny"}
-    
-  try {
-    const doc= await doctorModel.findOneAndUpdate(filter,req.body );
   
+  try {
+    const filter={Username:"Khalkhoola"}
+   // console.log(req.body.Email)
+   if(req.body.HourlyRate || req.body.Affiliation ){
+   const doc= await doctorModel.findOneAndUpdate(filter,req.body );
+   res.status(200).json(doc);
+   }
+   else {
+    const doc= await systemUserModel.findOneAndUpdate(filter,req.body );
     res.status(200).json(doc);
+   }
+ // console.log(req.body.HourlyRate);
+   
   } catch (error) {
     res.status(400).json({ error: error.message });
   }}
-  }
+ 
+
+  
   module.exports = {createDoctor,updateDoctor};
