b202457346445076d30717f8f334d570ad1025f7
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2023-10-06 22:16:12 +0200

    add getAppointments

--- backend/src/controllers/appointmentController.js
+++ backend/src/controllers/appointmentController.js
@@ -1,14 +1,25 @@
+const { trusted } = require("mongoose");
 const appointmentModel = require("../models/appointments");
 
 
+//filter by  date or status
 
 const getAppointments= async(req,res)=>{
-    const statusInput=false
-    if(statusInput!=null){
-        const result=await appointmentModel.find({Status:statusInput})
+    const current_type="Doctor" //session
+    const current_user="Touny"   //session
+    const statusInput=""   //khod input men el front end
+    const dateSearch= "2023-10-06"  //khod input men el front end
+    if(statusInput==="Pending" || statusInput==="Done"){
+    const statusValue=(statusInput==="Pending"?false:true)
+    const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user, Status:statusValue}) :await appointmentModel.find({PatientUsername:current_user,Status:statusValue}) 
+    res.status(200).send(result)
+}
+    else if(dateSearch!=null && !isNaN(new Date(dateSearch))){            //Gets date greater than or equal 
+        const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user,createdAt:{$gte:new Date(dateSearch)}}) : await appointmentModel.find({PatientUsername:current_user, createdAt:new Date(dateSearch)})
         res.status(200).send(result)
     }
-    else{
+  
+    else {
         res.status(404).send("Error occured")
     }
     
--- backend/src/models/patients.js
+++ backend/src/models/patients.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ImportFamObject = require("./familymembers");
-
 const patientSchema = new Schema(
   {
     Username: {
