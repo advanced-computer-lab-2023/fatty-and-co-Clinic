84cd3624f5efb523e24386595806c96d6830c762
Zeyadptd <zeyad.habashy02@gmail.com>
2023-10-06 15:52:46 +0300

    created functions and routes that find a single doctor by id and username

--- backend/src/controllers/doctorController.js
+++ backend/src/controllers/doctorController.js
@@ -1,3 +1,56 @@
 const doctorModel = require("../models/doctors");
+const mongoose = require("mongoose");
 
-module.exports = {};
+// get a doctor by ID
+const getDoctorByID = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ error: "Invalid ID" });
+    return;
+  }
+
+  try {
+    const doctor = await doctorModel.findById(id);
+    if (!doctor) {
+      res.status(404).json({ error: "Doctor not found" });
+      return;
+    }
+    const { Name, Speciality, Affiliation, EducationalBackground } = doctor;
+    const doctorDetails = {
+      Name,
+      Speciality,
+      Affiliation,
+      EducationalBackground,
+    };
+    res.status(200).json(doctorDetails);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// get a doctor by username
+const getDoctorByUsername = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const doctor = await doctorModel.findOne({ Username: username });
+    if (!doctor) {
+      res.status(404).json({ error: "Doctor not found" });
+      return;
+    }
+    const { Name, Speciality, Affiliation, EducationalBackground } = doctor;
+    const doctorDetails = {
+      Name,
+      Speciality,
+      Affiliation,
+      EducationalBackground,
+    };
+    console.log(doctor);
+    console.log(doctorDetails);
+    res.status(200).json(doctorDetails);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getDoctorByID, getDoctorByUsername };
--- backend/src/routes/doctors.js
+++ backend/src/routes/doctors.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { } = require("../controllers/doctorController");
+const {
+  getDoctorByID,
+  getDoctorByUsername,
+} = require("../controllers/doctorController");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Doctors");
 });
+
+router.get("/:id", getDoctorByID);
+
+router.get("/username/:username", getDoctorByUsername);
 
 module.exports = router;
