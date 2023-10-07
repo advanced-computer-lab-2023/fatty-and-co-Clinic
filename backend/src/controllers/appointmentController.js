87d63daea3b7b9386de79d9df3ea67e968451684
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2023-10-07 14:36:07 +0200

    update getAppointments Date part

--- backend/src/controllers/appointmentController.js
+++ backend/src/controllers/appointmentController.js
@@ -10,7 +10,7 @@ const getAppointments= async(req,res)=>{
     const current_type="Doctor" //session
     const current_user="Kholoud"   //session
     const statusInput=""   //khod input men el front end
-    const dateSearch= "2023-06-21"  //khod input men el front end
+    const dateSearch= "2023-06-21T02:53:00.000Z"  //khod input men el front end
     if(statusInput==="Pending" || statusInput==="Done"){
     const statusValue=(statusInput==="Pending"?false:true)
     const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user, Status:statusValue}) :await appointmentModel.find({PatientUsername:current_user,Status:statusValue}) 
@@ -19,8 +19,18 @@ const getAppointments= async(req,res)=>{
     else if(dateSearch!=null && !isNaN(new Date(dateSearch))){ 
         //Gets date on exact day
         const dateValue= new Date(dateSearch)        
-        const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user,Date:{$lt:new Date(dateValue.getFullYear,dateValue.getMonth,dateValue.getDay+1)},Date:{$gte:dateValue}}) : await appointmentModel.find({PatientUsername:current_user, Date:{$lt:new Date(dateValue.getFullYear,dateValue.getMonth,dateValue.getDay+1)},Date:{$gte:dateValue}})
-        res.status(200).send(result)
+        if(dateValue.getUTCHours()===0){
+            console.log("hena")
+            const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user,Date:{$lt:new Date(dateValue.getFullYear,dateValue.getMonth,dateValue.getDay+1)},Date:{$gte:dateValue}}) : await appointmentModel.find({PatientUsername:current_user, Date:{$lt:new Date(dateValue.getFullYear,dateValue.getMonth,dateValue.getDay+1)},Date:{$gte:dateValue}})
+            res.status(200).send(result)
+        }
+        else{
+            console.log("else part")
+            console.log(dateValue.getUTCHours())
+            const result= current_type==="Doctor"?await appointmentModel.find({DoctorUsername:current_user,Date:dateValue}) : await appointmentModel.find({PatientUsername:current_user, Date:dateValue})
+            res.status(200).send(result)
+        }
+      
     }
   
     else {
