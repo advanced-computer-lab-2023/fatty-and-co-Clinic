8e712732567023fa292ecce3e46f056ecf1261a6
Zeyadptd <zeyad.habashy02@gmail.com>
2023-10-09 21:41:36 +0300

    minor naming changes

--- frontend/src/views/Patient/viewDoctors/index.js
+++ frontend/src/views/Patient/viewDoctors/index.js
@@ -6,27 +6,24 @@ import { Flex } from "@chakra-ui/react";
 import { API_PATHS } from "API/api_paths";
 
 function ViewDoctors() {
-  const [mdata, setData] = useState([{}]);
+  const [data, setData] = useState([{}]);
   const id = "6521bece68537b0c5336b14a";
   useEffect(() => {
     fetch(API_PATHS.viewAllDoctors + id)
       .then((response) => response.json())
-      .then((mdata) => setData(mdata));
+      .then((data) => setData(data));
   }, []);
 
   return (
     //search bar & filter
 
-    console.log("hello" + mdata),
-    (
-      <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
-        <DoctorsTable
-          title={"Available Doctors"}
-          captions={["Name", "Speciality", "Cost"]}
-          data={mdata}
-        />
-      </Flex>
-    )
+    <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
+      <DoctorsTable
+        title={"Available Doctors"}
+        captions={["Name", "Speciality", "Cost"]}
+        data={data}
+      />
+    </Flex>
   );
 }
 
