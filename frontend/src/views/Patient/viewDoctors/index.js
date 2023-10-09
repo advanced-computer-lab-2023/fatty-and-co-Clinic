4009850aef7c05714fc3e522ee295dc71242034e
Shorok02 <shorokabdulraof@gmail.com>
2023-10-09 21:32:23 +0300

    merge blabla

--- frontend/src/views/Patient/viewDoctors/index.js
+++ frontend/src/views/Patient/viewDoctors/index.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "components/Navbars/SearchBar/SearchBar";
 import DoctorsRow from "components/Tables/DoctorsRow";
 import DoctorsTable from "./components/DoctorsTable";
 import { Flex } from "@chakra-ui/react";
-import { DoctorListContextProvider } from "../../../context/DoctorsContext";
+import { API_PATHS } from "API/api_paths";
 
-function viewDoctors() {
+function ViewDoctors() {
+  const [mdata, setData] = useState([{}]);
+  const id = "6521bece68537b0c5336b14a";
+  useEffect(() => {
+    fetch(API_PATHS.viewAllDoctors + id)
+      .then((response) => response.json())
+      .then((mdata) => setData(mdata));
+  }, []);
+
   return (
-    <DoctorListContextProvider>
-      //search bar & filter
+    //search bar & filter
+
+    console.log("hello" + mdata),
+    (
       <Flex direction="column" pt={{ base: "120px", md: "75px" }}>
         <DoctorsTable
           title={"Available Doctors"}
           captions={["Name", "Speciality", "Cost"]}
-          data={[
-            {
-              Name: "JiraLogo",
-              Speciality: "Add the New Pricing Page",
-              Cost: "990",
-            },
-          ]}
+          data={mdata}
         />
       </Flex>
-    </DoctorListContextProvider>
+    )
   );
 }
 
-export default viewDoctors;
+export default ViewDoctors;
