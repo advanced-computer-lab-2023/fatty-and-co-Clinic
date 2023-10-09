0692dd5b32015b31f870faa20c4670b284f426e3
Shorok02 <shorokabdulraof@gmail.com>
2023-10-09 21:20:27 +0300

    viewAllDoctorsMethod done

--- frontend/src/API/api_paths.js
+++ frontend/src/API/api_paths.js
@@ -1,8 +1,16 @@
 const API_BASE = "http://localhost:8000/";
 
 export const API_PATHS = {
+  //package
   packages: API_BASE + "package/packages/",
   addPackage: API_BASE + "package/addPackage/",
   deletePackage: API_BASE + "package/deletePackage/",
   updatePackage: API_BASE + "package/updatePackage/",
+
+
+  //patient
+  viewAllDoctors: API_BASE + "patient/view/doctors/",
+  
+
+
 };
