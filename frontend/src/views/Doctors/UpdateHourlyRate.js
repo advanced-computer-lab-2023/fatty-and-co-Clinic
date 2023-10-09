3a50e43aadff0e282fe34ab0180159ba0c454b82
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2023-10-09 15:30:19 +0200

    update again :)

--- frontend/src/views/Doctors/UpdateHourlyRate.js
+++ frontend/src/views/Doctors/UpdateHourlyRate.js
@@ -63,124 +63,25 @@ function UpdateHourlyRate() {
                 fontSize='sm'
                 type='number'
                 size='lg'
-                backgroundColor="white"
+                disabled="true"
+                pointerEvents="none"
+                backgroundColor="ref"
 
               />
               <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
                 New hourly rate
               </FormLabel>
-              <Select >
-               <option value>1</option>
-               <option value>2</option>
-               <option value>3</option>
-               <option value>4</option>
-               <option value>5</option>
-               <option value>6</option>
-               <option value>7</option>
-               <option value>8</option>
-               <option value>9</option>
-               <option value>10</option>
-               <option value>11</option>
-               <option value>12</option>
-               <option value>13</option>
-               <option value>14</option>
-               <option value>15</option>
-               <option value>16</option>
-               <option value>17</option>
-               <option value>18</option>
-               <option value>19</option>
-               <option value>20</option>
-               <option value>21</option>
-               <option value>22</option>
-               <option value>23</option>
-               <option value>24</option>
-               <option value>25</option>
-               <option value>26</option>
-               <option value>27</option>
-               <option value>28</option>
-               <option value>29</option>
-               <option value>30</option>
-               <option value>31</option>
-               <option value>32</option>
-               <option value>33</option>
-               <option value>34</option>
-               <option value>35</option>
-               <option value>36</option>
-               <option value>37</option>
-               <option value>38</option>
-               <option value>39</option>
-               <option value>40</option>
-               <option value>41</option>
-               <option value>42</option>
-               <option value>43</option>
-               <option value>44</option>
-               <option value>45</option>
-               <option value>46</option>
-               <option value>47</option>
-               <option value>48</option>
-               <option value>49</option>
-               <option value>50</option>
-               <option value>51</option>
-               <option value>52</option>
-               <option value>53</option>
-               <option value>54</option>
-               <option value>55</option>
-               <option value>56</option>
-               <option value>57</option>
-               <option value>58</option>
-               <option value>59</option>
-               <option value>60</option>
-               <option value>61</option>
-               <option value>62</option>
-               <option value>63</option>
-               <option value>64</option>
-               <option value>65</option>
-               <option value>66</option>
-               <option value>67</option>
-               <option value>68</option>
-               <option value>69</option>
-               <option value>70</option>
-               <option value>71</option>
-               <option value>72</option>
-               <option value>73</option>
-               <option value>74</option>
-               <option value>75</option>
-               <option value>76</option>
-               <option value>77</option>
-               <option value>78</option>
-               <option value>79</option>
-               <option value>80</option>
-               <option value>81</option>
-               <option value>82</option>
-               <option value>83</option>
-               <option value>84</option>
-               <option value>84</option>
-               <option value>86</option>
-               <option value>87</option>
-               <option value>88</option>
-               <option value>89</option>
-               <option value>90</option>
-               <option value>91</option>
-               <option value>92</option>
-               <option value>93</option>
-               <option value>94</option>
-               <option value>95</option>
-               <option value>96</option>
-               <option value>97</option>
-               <option value>98</option>
-               <option value>99</option>
-               <option value>100</option>
-              </Select>
-              {/* <Input
+              
+              <Input id="Value"
                 borderRadius='15px'
                 mb='36px'
                 fontSize='sm'
                 type='number'
-                placeholder='Select new hourly rate'
-                // value="999999"
-                step="1"
-                
-              /> */}
+                placeholder='New hourly rate'
+                max="10000"
+                maxlength="5"
+       
+              />
 
               <Button
                 fontSize='20px'
