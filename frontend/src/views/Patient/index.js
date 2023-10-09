279e5060b26b972f37ac9bd42d6e8414a0b7322c
Shorok02 <shorokabdulraof@gmail.com>
2023-10-09 16:29:37 +0300

    started patient dashboard
    added doctor table

---
+++ frontend/src/views/Patient/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import  SearchBar  from "components/Navbars/SearchBar/SearchBar";
+import DoctorsRow from "components/Tables/DoctorsRow";
+import DoctorsTable from "./components/DoctorsTable";
+import { Flex } from "@chakra-ui/react";
+
+
+
+function Patient(){
+
+ 
+
+
+
+
+    return (
+
+        //search bar & filter
+        <Flex direction='column' pt={{ base: "120px", md: "75px" }}>
+        <DoctorsTable
+        title={"Available Doctors"}
+        captions={["Name", "Speciality", "Cost"]}
+        data= {[{
+                Name: "JiraLogo",
+                Speciality: "Add the New Pricing Page",
+                Cost: "990"
+                }]}
+        />
+        </Flex>
+    )
+
+}
+
+export default Patient;
