608752c0af7c4abf2b050376f55859a0bab11610
Shorok02 <shorokabdulraof@gmail.com>
2023-10-09 17:59:26 +0300

    added fetch data of doctors view

--- frontend/src/views/Patient/index.js
+++ frontend/src/views/Patient/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import  SearchBar  from "components/Navbars/SearchBar/SearchBar";
 import DoctorsRow from "components/Tables/DoctorsRow";
 import DoctorsTable from "./components/DoctorsTable";
@@ -7,24 +7,35 @@ import { Flex } from "@chakra-ui/react";
 
 
 function Patient(){
-
+    useEffect(() => {
+        const myData = async() => {
+            const response = await fetch("patient/view/doctors/:id");
+            const data = await response.json();
+            if (response.ok) {
+              console.log(packages);
+            } else {
+              console.log(data);
+            }
+        }
+    })
  
-
-
-
-
     return (
 
         //search bar & filter
+
+        
         <Flex direction='column' pt={{ base: "120px", md: "75px" }}>
         <DoctorsTable
         title={"Available Doctors"}
         captions={["Name", "Speciality", "Cost"]}
-        data= {[{
-                Name: "JiraLogo",
-                Speciality: "Add the New Pricing Page",
-                Cost: "990"
-                }]}
+        data= {
+            // [{
+            //     Name: "JiraLogo",
+            //     Speciality: "Add the New Pricing Page",
+            //     Cost: "990"
+            //     }]
+            myData
+            }
         />
         </Flex>
     )
