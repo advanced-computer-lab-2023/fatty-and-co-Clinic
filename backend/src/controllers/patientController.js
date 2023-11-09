29cb2048eb38d82bfa4b0c18b24ddb3d74e7fb90
kholoudkhaleddaly <127565809+kholoudkhaleddaly@users.noreply.github.com>
2023-11-09 16:52:00 +0200

    Edited cancell subscripition

--- backend/src/controllers/patientController.js
+++ backend/src/controllers/patientController.js
@@ -767,7 +767,6 @@ module.exports = {
   getEmergencyContact,
   subscribepackagefamilymem,
   payForFamSubscription,
-  cancelSubscription,
   cancelSubscriptionfamilymember,
   payForSubscription,
   viewHealthPackagewithstatus,
