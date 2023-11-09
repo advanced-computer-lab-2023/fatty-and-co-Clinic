fd7ed509b82bb0697b0e479683ff52fb1811ac5d
mariam-alaa20031 <109764181+mariam-alaa20031@users.noreply.github.com>
2023-11-09 16:28:23 +0100

    Update patientController.js

--- backend/src/controllers/patientController.js
+++ backend/src/controllers/patientController.js
@@ -271,6 +271,7 @@ const payForFamSubscription= async(req,res)=>{
     console.log(curr_user)
     console.log(subscription)
   
+
     if(relative.FamilyMem!=null){
       res.status(400).send({message:"Cannot subscribe for another system user!"})
     }
@@ -304,8 +305,8 @@ const payForFamSubscription= async(req,res)=>{
     const formattedDate4 = `${year4}-${month4}-${day4}`;
     const max= subDiscount.PackageName!=null && subDiscount.Status==="Subscribed"?subDiscount.PackageName.Family_Discount:0 
     const amount=subscription.PackageName.price-max
-  
     if(subscription.Status==="Subscribed" && formattedDate===formattedDate3 ){
+      console.log(patient.Wallet)
       if(patient.Wallet>amount ){
       const updatePat= await patientModel.findOneAndUpdate({Username:curr_user},{Wallet:patient.Wallet-amount})
       res.status(200).json(updatePat)
