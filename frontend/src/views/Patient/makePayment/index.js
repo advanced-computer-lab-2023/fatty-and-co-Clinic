14c5cd8f5623dd5d9105a5c9488f52b9b2963269
Rafeeq Bassem <ultimaterafy@gmail.com>
2023-11-10 13:29:21 +0200

    modifications to the cardpayment page

--- frontend/src/views/Patient/makePayment/index.js
+++ frontend/src/views/Patient/makePayment/index.js
@@ -1,12 +1,17 @@
-import Stripe from "stripe";
-import StripeContainer from "./components/StripeContainer";
+import react from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import PaymentForm from "./components/PaymentForm";
 
-function MakePayment() {
+const PUBLIC_KEY =
+  "pk_test_51O9tZeDlw6jDceOALILn3gb3eOAzBSHSuIQKUqUxTX8eSdJn36QpsxAqKWskgVA5TNzo5Zx3OAQbn3I7ZjZHxSZX00M5RKDnCM";
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+export default function MakePayment({ amount }) {
   return (
-    <div>
-      <StripeContainer amount={10} />
-    </div>
+    <Elements stripe={stripeTestPromise}>
+      <PaymentForm amount={amount} />
+    </Elements>
   );
 }
-
-export default MakePayment;
