d41556afd8ddf4898ca0cf457f05f788cae07f08
Rafeeq Bassem <ultimaterafy@gmail.com>
2023-12-04 16:29:40 +0200

    prescription controller

---
+++ backend/src/controllers/prescriptionController.js
@@ -0,0 +1,9 @@
+const { default: mongoose } = require("mongoose");
+
+const Prescription = require("../models/prescriptionModel");
+const Patient = require("../models/patientModel");
+const Doctor = require("../models/doctorModel");
+const Appointment = require("../models/appointmentModel");
+const { default: axios } = require("axios");
+
+
