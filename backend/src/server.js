ce49f6ac0516ee9c8dad91e34a12a5d8800dad41
Shorok02 <shorokabdulraof@gmail.com>
2023-12-17 00:45:37 +0200

    doctor chat pharm set up

--- backend/src/server.js
+++ backend/src/server.js
@@ -58,6 +58,8 @@ const io = socketIo(server, {
   cors: { origin: `http://localhost:3000` },
 });
 
+const pharmacySocket = require('socket.io-client')('http://localhost:7000');
+
 io.on("connection", (socket) => {
   //upon connection
   console.log("New client connected");
@@ -93,6 +95,11 @@ io.on("connection", (socket) => {
       // console.log("notification sent")
     }
   });
+
+  socket.on("sendPharmacy", () => {
+    console.log("sendPharmacy");
+    pharmacySocket.emit("receivedMessage", "HusseinPha2@");
+  })
 
   //upon disconnection
   socket.on("disconnect", () => {
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -46,7 +46,7 @@ import DoctorProfile from "views/Doctors/viewProfile";
 import chatWithDoc from "views/Patient/chatwithDoc";
 import ChatWithPatient from "views/Doctors/chat/chatwithPat";
 import DashboardPat from "views/Dashboard/DashboardPat";
-
+import ChatWithPharmacist from "views/Doctors/chat/chatwithPhar";
 import VideoCallPage from "views/Patient/VideoCallTemp";
 import {
   HomeIcon,
@@ -101,6 +101,15 @@ var dashRoutes = [
     component: VideoCallPage,
     layout: "/doctor",
   },
+  {
+    path: "/chatWithPharm",
+    name: "chatWithPharm",
+    icon: <HomeIcon color="inherit" />,
+    component: ChatWithPharmacist,
+    layout: "/doctor",
+  },
+
+
   {
     path: "/package",
     name: "Package",
